9ea138497a290c2c0badf0445e5aeb92e6c63f9a
kychanbp <ust.johnchan@gmail.com>
2019-10-03 21:20:28 +0800

    combined SHT31 and BMP180

---
+++ weatherStation_3/src/main.cpp
@@ -0,0 +1,90 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SFE_BMP180.h>
+#include "Adafruit_SHT31.h"
+
+// Connect VCC of the BMP085 sensor to 3.3V (NOT 5.0V!)
+// Connect GND to Ground
+// Connect SCL to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5
+// Connect SDA to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4
+// EOC is not used, it signifies an end of conversion
+// XCLR is a reset pin, also not used here
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+SFE_BMP180 pressure;
+  
+#define ALTITUDE 1655.0 
+
+void setup() {
+  Serial.begin(9600);
+
+  if (pressure.begin())
+    Serial.println("BMP180 init success");
+  else
+  {
+    // Oops, something went wrong, this is usually a connection problem,
+    // see the comments at the top of this sketch for the proper connections.
+
+    Serial.println("BMP180 init fail\n\n");
+    while(1); // Pause forever.
+  } 
+
+  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+}
+  
+void loop() 
+{
+  char status;
+  double T,P,p0,a;
+  float t = sht31.readTemperature();
+  float h = sht31.readHumidity();
+
+  //temperature
+  if (! isnan(t)) {  // check if 'is not a number'
+    Serial.print("Temp *C = "); Serial.println(t);
+  } else { 
+    Serial.println("Failed to read temperature");
+  }
+  
+  if (! isnan(h)) {  // check if 'is not a number'
+    Serial.print("Hum. % = "); Serial.println(h);
+  } else { 
+    Serial.println("Failed to read humidity");
+  }
+
+  //pressure
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    delay(status);
+
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        delay(status);
+
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          p0 = pressure.sealevel(P,ALTITUDE); // we're at 1655 meters (Boulder, CO)
+          Serial.print("relative (sea-level) pressure: ");
+          Serial.print(p0,2);
+          Serial.print(" mb, ");
+          Serial.println("");
+        }
+        else Serial.println("error retrieving pressure measurement\n");
+      }
+      else Serial.println("error starting pressure measurement\n");
+    }
+    else Serial.println("error retrieving temperature measurement\n");
+  }
+  else Serial.println("error starting temperature measurement\n");
+
+  delay(1000);  // Pause for 5 seconds.
+}
